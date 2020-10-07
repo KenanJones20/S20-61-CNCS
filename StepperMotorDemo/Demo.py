a468d5ca74e9eb0c99c0c640b89ddc5a6aebcc70
KenanJones20 <71467092+KenanJones20@users.noreply.github.com>
2020-10-07 15:34:50 -0500

    Working version of Demo.py
    Working code, can spin 5 motors. As of 10-07-2020

--- StepperMotorDemo/Demo.py
+++ StepperMotorDemo/Demo.py
@@ -3,51 +3,54 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
+FREQ = 500 #pwm frequency
+
 #GPIO pin names
-X_Axis_Dir = 18
-Y_Axis_Dir = 23
-Z_Axis_Dir = 8
-Planter_Dir = 10
+X_Axis_Dir = 10
+Y_Axis_Dir = 9
+Z_Axis_Dir = 11
+Planter_Dir = 15
 
 #GPIO pin definitions
-GPIO.setup(17, GPIO.OUT)
-X_Axis_PWM = GPIO.PWM(17,1000)
-GPIO.setup(22, GPIO.OUT)
-Y_Axis_PWM = GPIO.PWM(22,1000)
+GPIO.setup(25, GPIO.OUT)
+X_Axis_PWM = GPIO.PWM(25,FREQ)
+GPIO.setup(8, GPIO.OUT)
+Y_Axis_PWM = GPIO.PWM(8,FREQ)
 GPIO.setup(7, GPIO.OUT)
-Z_Axis_PWM = GPIO.PWM(7,1000)
-GPIO.setup(9, GPIO.OUT)
-Planter_PWM = GPIO.PWM(9,1000)
+Z_Axis_PWM = GPIO.PWM(7,FREQ)
+GPIO.setup(18, GPIO.OUT)
+Planter_PWM = GPIO.PWM(18,FREQ)
 GPIO.setup((X_Axis_Dir,Y_Axis_Dir,Z_Axis_Dir,Planter_Dir),
            GPIO.OUT, initial = GPIO.LOW)
 
 #move X axis stepper motors
 X_Axis_PWM.start(50)
-sleep(1)
+sleep(2)
 X_Axis_PWM.stop()
 
 #move Y axis stepper motor
 Y_Axis_PWM.start(50)
-sleep(1)
+sleep(2)
 Y_Axis_PWM.stop()
 
 #move Z axis stepper motor
 Z_Axis_PWM.start(50)
-sleep(1)
+sleep(2)
 Z_Axis_PWM.stop()
 
 #move planter stepper motor
 Planter_PWM.start(50)
-sleep(1)
+sleep(2)
 Planter_PWM.stop()
 
 #move all the motors backward
-GPIO.output((X_Axis_Dir,Y_Axis_Dir,Z_Axis_Dir,Planter_Dir), GPIO.HIGH)
+GPIO.output((X_Axis_Dir,Y_Axis_Dir,Z_Axis_Dir,
+             Planter_Dir), GPIO.HIGH)
 X_Axis_PWM.start(50)
 Y_Axis_PWM.start(50)
 Z_Axis_PWM.start(50)
 Planter_PWM.start(50)
-sleep(1)
+sleep(3)
 X_Axis_PWM.stop()
 Y_Axis_PWM.stop()
 Z_Axis_PWM.stop()
